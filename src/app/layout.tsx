4f21d22ba52237ad6f94ff3d1a9a4a425e3bb1b8
Arjun Rajendran <ar5unrajendran@gmail.com>
2025-10-08 11:41:11 +0400

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
 import { Layout, FixedPlugin } from "@/components";
 import I18nProvider from "@/i18n/I18nProvider";
-
-const roboto = Roboto({
-  subsets: ["latin", "cyrillic"],
-  weight: ["300", "400", "500", "700", "900"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "CometByte - Transform Your Digital Future",
@@ -24,6 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Fonts - Roboto with Latin and Cyrillic support */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap&subset=latin,cyrillic" 
+          rel="stylesheet" 
+        />
         <script
           defer
           data-site="YOUR_DOMAIN_HERE"
@@ -31,7 +31,7 @@ export default function RootLayout({
         ></script>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
-      <body className={roboto.className}>
+      <body style={{ fontFamily: 'Roboto, sans-serif' }}>
         <I18nProvider>
           <Layout>
             {children}
